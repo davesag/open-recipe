f5d7e97ed2b799a6e21b82b8ee172f659a3af47c
davesag <davesag@gmail.com>
2013-02-09 18:43:24 +1100

    building in jQuery validation

--- public/scripts/form_to_jquery_ui.js
+++ public/scripts/form_to_jquery_ui.js
@@ -1,8 +1,46 @@
+// the ui.form_expander plugin is applied to a jQuery form object.
+// It wraps all input fields in a label to name the field, and a label to carry any validation errors.
+// It does not do this for input fields within a span or td cell however.
+// It adds a hover handler to all fields with the 'hover' class attached.
+// It accepts optional init options as follows:
+//     {buttons: {
+//     'choose-photo': {name: "Choose Photo", handler: 'choose-photo'},
+//     'save-recipe': {name: "Save Recipe", handler: 'save-recipe'},
+//     'cancel': {name: "Cancel", handler: 'cancel'}
+//     },
+//     fields: {
+//       'recipe-name': {name: "Name", validation: {
+//         required: true
+//        },
+//        messages: 'You must provide a name.'
+//       },
+//       'serves': {name: "Serves", validation: {
+//         required: true,
+//         number: true
+//        },
+//        messages: {
+//          required: 'You must say how many people this recipe serves.',
+//          number: 'The serving suggestion must be a number.'
+//        }
+//       },
+//       'description': {name: "Description"},
+//       'prep-time': {name: "Prep Time"},
+//       'cooking-time': {name: "Cooking Time"},
+//       'requirements': {name: "Requirements"},
+//       'the-method': {name: "Method"},
+//       'ingredients': {name: "Ingredients"}
+//     }}
 $(function() {
-  $.widget("ui.form",{
+  $.widget("ui.form_expander",{
     _init: function(){
+      console.log ('form_expander init.', this.option());
+      var options = this.option();  // might want to do ome checking of this now.
       var object = this;
       var form = this.element;
+      
+      var validation_rules = {};
+      var validation_messages = {};
+      
       var inputs = form.find("input , select ,textarea");
 
       form.find("fieldset").addClass("ui-widget-content");
@@ -11,13 +49,15 @@ $(function() {
 
       $.each(inputs,function(){
         var field = $(this);
-        field.addClass('ui-state-default ui-corner-all');
-        if (field.attr('name') === '') {
-          field.attr('name', field.attr('id'));
-        }
         var field_id = field.attr('id');
+        field.addClass('ui-state-default ui-corner-all');
+        if (typeof field.attr('name') === 'undefined' || field.attr('name') === '') {
+          field.attr('name', field_id); // so the jQuery Validator plugin works.
+        } else if (field.attr('name') !== field_id) field_id = field.attr('name');
+        // console.log("field name = ", field.attr('name'));
+        var fModel = options.fields[field.attr('name')];
         if (!(field.parent().is("span") || field.parent().is("td"))) {
-          $('<label for="'+ field_id +'">' + field_names[field_id] + '</label>').insertBefore(field);
+          $('<label for="'+ field_id +'">' + fModel.name + '</label>').insertBefore(field);
           $('<label for="'+ field_id +'" generated="true" class="error"></label>').insertAfter(field);
         }
         if(field.is(":reset ,:submit")) object.buttons(this);
@@ -27,13 +67,32 @@ $(function() {
         else if(field.is("select")) object.selector(this);
 
         if(field.hasClass("date")) field.datepicker();
+        
+        // apply any validation rules here.
+        if (typeof fModel !== 'undefined' && fModel.validation != null) {
+          validation_rules[field.attr('name')] = fModel.validation;
+          if (fModel.messages != null) validation_messages[field.attr('name')] = fModel.messages;
+        }
       });
+
+      var vr = {
+        debug: true,
+        submitHandler: function(f) {
+          // do nothing
+          console.log("form submit called for ", f);
+        },
+        rules: validation_rules,
+        messages: validation_messages
+      }
+      console.log("setting validation rules:", vr);
+      form.validate(vr);  // activate the validation handler.  Add rules below.
 
       $(".hover").hover(function(){
         $(this).addClass("ui-state-hover");
       }, function(){
         $(this).removeClass("ui-state-hover");
       });
+      return object;
     },
     textelements: function(element){
       $(element).bind({
@@ -47,21 +106,16 @@ $(function() {
 
     },
     buttons:function(element) {
-      if ($(element).is(":submit")) {
-        $(element).addClass("ui-priority-primary ui-corner-all ui-state-disabled hover");
-        $(element).val(button_names[$(element).attr('id')]);
-        $(element).bind("click",function(event) {
-          event.preventDefault();
-        });
-      } else if ($(element).is(":reset")) $(element).addClass("ui-priority-secondary ui-corner-all hover");
-    
-      $(element).bind('mousedown mouseup', function() {
-        $(this).toggleClass('ui-state-active');
-      });
+      var elm = $(element);
+      if (elm.is(":submit")) {
+        elm.val(this.option().buttons[elm.attr('id')].name);
+        elm.button().click(this.option().buttons[elm.attr('id')].handler);
+      } else if (elm.is(":reset")) elm.addClass("ui-priority-secondary ui-corner-all hover");
+      if (elm.is(":disabled")) elm.addClass('ui-state-disabled');
     },
     checkboxes: function(element){
-      $(element).parent("label").after("<span/>");
-      var parent =  $(element).parent("label").next();
+      $(element).wrap("<span/>");
+      var parent =  $(element).parent();
       $(element).addClass("ui-helper-hidden");
       parent.css({width:16,height:16,display:"block"});
       parent.wrap("<span class='ui-state-default ui-corner-all' style='display:inline-block;width:16px;height:16px;margin-right:5px;'/>");
@@ -73,33 +127,19 @@ $(function() {
       });
     },
     radio: function(element){
-      $(element).parent("label").after("<span/>");
-      var parent =  $(element).parent("label").next();
+      $(element).wrap("<span/>");
+      var parent =  $(element).parent();
       $(element).addClass("ui-helper-hidden");
-      parent.addClass("ui-icon ui-icon-radio-off");
-      parent.wrap("&lt;span class='ui-state-default ui-corner-all' style='display:inline-block;width:16px;height:16px;margin-right:5px;'/>");
-      parent.parent().addClass('hover');
-      parent.parent("span").click(function(event){
+      parent.addClass("ui-icon ui-icon-radio-off hover");
+      parent.wrap("<span class='ui-state-default ui-corner-all' style='display:inline-block;width:16px;height:16px;margin-right:5px;'/>");
+      parent.click(function(event){
         $(this).toggleClass("ui-state-active");
         parent.toggleClass("ui-icon-radio-off ui-icon-bullet");
         $(element).click();
       });
     },
     selector: function(element){
-      var parent = $(element).parent();
-      parent.css({"display":"block",width:140,height:21}).addClass("ui-state-default ui-corner-all");
-      $(element).addClass("ui-helper-hidden");
-      parent.append("<span id='labeltext' style='float:left;'></span><span style='float:right;display:inline-block' class='ui-icon ui-icon-triangle-1-s' ></span>");
-      parent.after("<ul class=' ui-helper-reset ui-widget-content ui-helper-hidden' style='position:absolute;z-index:50;width:140px;' ></ul>");
-      $.each($(element).find("option"),function(){
-        $(parent).next("ul").append("<li class='hover'>"+$(this).html()+"</li>");
-      });
-      $(parent).next("ul").find("li").click(function(){ $("#labeltext").html($(this).html());
-        $(element).val($(this).html());
-      });
-      $(parent).click(function(event){ $(this).next().slideToggle('fast');
-        event.preventDefault();
-      });
+      // do nothing.
     }
   })
 });
--- public/scripts/recipe_form_handlers.js
+++ public/scripts/recipe_form_handlers.js
@@ -14,110 +14,65 @@ var ingredient_name_autocompleter = {
 };
 
 var button_handlers = {
-  "choose-photo" : function () {
+  "choose-photo" : function (event) {
+    event.preventDefault();
     console.log('choose a recipe photo from your facebook photos.');  // enhance this later
     // see example at http://code.google.com/p/facebook-photo-picker/
     // and a jQuery one at https://github.com/seanhellwig/jQuery-Facebook-Multi-Photo-Selector
   },
-  "save-recipe" : function () {
+  "save-recipe" : function (event) {
+    event.preventDefault();
     // console.log("saving recipe.");
     // go through the form and collect all the fields into a
     // recipe object.
-    if (validate_recipe($("form"))) {
-      var ingredients = new Array();
-      var number_of_ingredients = $('#ingredients .ingredient').length
-      for (i = 0; i < number_of_ingredients ; i++ ) {
-        var n = $('#ingredient'+i).val();
-        var a = $('#amount'+i).val();
-        var u = $('#unit'+i).val();
-        console.log ("ingredient = " + n + ", amount = " + a + ", unit = " + u);
-        if (n != '') {
-          q = new Quantity(a, u);
-          ingredients.push(new ActiveIngredient(n,q));
-        }
+    var ingredients = new Array();
+    var number_of_ingredients = $('#ingredients .ingredient').length
+    for (i = 0; i < number_of_ingredients ; i++ ) {
+      var n = $('#ingredient'+i).val();
+      var a = $('#amount'+i).val();
+      var u = $('#unit'+i).val();
+      console.log ("ingredient = " + n + ", amount = " + a + ", unit = " + u);
+      if (n != '') {
+        q = new Quantity(a, u);
+        ingredients.push(new ActiveIngredient(n,q));
       }
-      // merge cooking and prep time fields into a nice time string
-      // in dd:hh:mm format
-      var ct = to_seconds($('#cooking-time-days').val(),
-                          $('#cooking-time-hours').val(),
-                          $('#cooking-time-minutes').val());
-      var pt = to_seconds($('#prep-time-days').val(),
-                          $('#prep-time-hours').val(),
-                          $('#prep-time-minutes').val());
-      var r = new Recipe(recipe_id, $('#recipe-name').val(), parseInt($('#serves').val()), ct, pt, $('#description').val(),
-          $('#the_method').val(), $('#requirements').val(), ingredients, null, null); // ignore tags and meal for now.
-      var req = new Recipe_Request(r);
-      console.log('created recipe request object', req);
-
-      // now fire off an AJAX post to the server.
-      $.post(req.post_path(), req.post_data(), function(data){
-        console.log('debug', data)
-        if (!data['success']) {
-          // there was an error.
-          console.log('error', data['error']);
-          // do something to tell the user about this error.
-        } else {
-          // there is a message
-          console.log('message', data['message']);
-          // perhaps do something with this message.
-          location.href = '/';
-        }
-      }).error(function() {
-          alert(ERROR_MESSAGES['server']);
-      });
     }
+    // merge cooking and prep time fields into a nice time string
+    // in dd:hh:mm format
+    var ct = to_seconds($('#cooking-time-days').val(),
+                        $('#cooking-time-hours').val(),
+                        $('#cooking-time-minutes').val());
+    var pt = to_seconds($('#prep-time-days').val(),
+                        $('#prep-time-hours').val(),
+                        $('#prep-time-minutes').val());
+    var r = new Recipe(recipe_id, $('#recipe-name').val(), parseInt($('#serves').val()), ct, pt, $('#description').val(),
+        $('#the_method').val(), $('#requirements').val(), ingredients, null, null); // ignore tags and meal for now.
+    var req = new Recipe_Request(r);
+    console.log('created recipe request object', req);
+
+    // now fire off an AJAX post to the server.
+    $.post(req.post_path(), req.post_data(), function(data){
+      console.log('debug', data)
+      if (!data['success']) {
+        // there was an error.
+        console.log('error', data['error']);
+        // do something to tell the user about this error.
+      } else {
+        // there is a message
+        console.log('message', data['message']);
+        // perhaps do something with this message.
+        // location.href = '/';
+      }
+    }).error(function() {
+        alert(ERROR_MESSAGES['server']);
+    });
   },
-  "cancel" : function () {
+  "cancel" : function (event) {
+    event.preventDefault();
     // check if there have been any changes.
     console.log("Cancel clicked");
     location.href = '/';
   }
-}
-
-var field_errors = {};
-
-var validation_rules = {
-  'required': function(field_name) {
-    if ($("#" + field_name).val() !== '') return true;
-    field_errors[field_name] = 'field-required';
-    return false;
-  },
-  'integer': function(field_name) {
-    var fv = $("#" + field_name).val();
-    if (fv === '') return true; // it's allowed to be empty.
-    if (fv === '' + parseInt(fv)) return true;
-    field_errors[field_name] = 'field-integer';
-    return false
-  },
-}
-
-function validate_recipe(form) {
-  // name field must contain some text.
-  validation_rules['required']('recipe-name');
-  if (validation_rules['required']('serves')) {
-    validation_rules['integer']('serves');
-  }
-  validation_rules['integer']('prep-time-days');
-  validation_rules['integer']('prep-time-hours');
-  validation_rules['integer']('prep-time-minutes');
-  validation_rules['integer']('cooking-time-days');
-  validation_rules['integer']('cooking-time-hours');
-  validation_rules['integer']('cooking-time-minutes');
-
-  var error_message = '';
-  for (i in field_errors) {
-    // console.log('found error ', i);
-    // console.log('found error['+i+'] ', field_errors[i]);
-    // console.log('found ERROR_MESSAGES['+field_errors[i]+'] ', ERROR_MESSAGES[field_errors[i]]);
-    error_message = error_message +
-      i + ': ' + ERROR_MESSAGES[field_errors[i]]+'\r'
-  }
-  // console.log('Object.keys(field_errors).length', Object.keys(field_errors).length);
-  if (Object.keys(field_errors).length > 0) {
-    alert(error_message);
-    return false;
-  }
-  return true;
 }
 
 $(function(){
@@ -130,13 +85,4 @@ $(function(){
     $(default_allowed_unit_options).appendTo('#ingredients .unit-menu');
   }
   $('#ingredients .ingredient').autocomplete(ingredient_name_autocompleter);
-
-  $("input:submit").button({
-    create: function(event, ui) {
-      $(this).val(button_names[$(this).attr('id')]);
-    }
-  }).click(function(event) {
-    event.preventDefault();
-    button_handlers[$(this).attr('id')]();
-  });
 });
